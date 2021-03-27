44e6b93f6507ca9f5297db2adbd76ee090121e50
jennaturunen <jenna.turunen2@gmail.com>
2021-03-27 21:08:40 +0200

    added test get route

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(express.urlencoded({ extended: false })); // for parsing html form x-www
 // and/or app.use(express.json()); // for parsing application/json
 app.use('/cat', require('./route'));
 
+app.get('/', (req, res) => {
+  console.log('get/');
+  res.send('Hello to demo node and mongo, try/cath route');
+});
+
 db.on('connected', () => {
   app.listen(3000, () => {
     console.log('express server started on port 3000');
